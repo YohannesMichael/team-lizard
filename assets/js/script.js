51c3a6684556aec6a74fc0738e4677726a76e2a6
cloud444 <y_michael@outlook.com>
2024-03-29 02:37:17 +1100

    added fetchQuote() for single author

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,52 @@ const genreInput = document.getElementById('genre').value;
 
 const genreUrl = `https://openlibrary.org/search/${genreInput}.json`;
 
+let authorArr = [];
 async function fetchAuthor() {
     try {
         const response = await fetch('https://openlibrary.org/subjects/love.json');
+
         if (!response.ok) {
             throw new Error("Hmmm, I don't know this genre.");
         }
+
         const data = await response.json();
-        console.log(data.works);
         const bookTitles = data.works;
-        let authorArr = [];
-        bookTitles.forEach(element => {
-            console.log(element.authors[0].name);
-            authorArr.push(element.authors[0].name);
+
+        bookTitles.forEach(title => {
+            console.log(title.authors[0].name);
+            authorArr.push(title.authors[0].name);
         });
+
         console.log(authorArr);
     }
+
     catch (error) {
         console.error(error);
     }
 }
 
 fetchAuthor();
+
+async function fetchQuote() {
+    try {
+        const response = await fetch('https://api.quotable.io/quotes?author=william-shakespeare');
+
+        if (!response.ok) {
+            throw new Error("Hmmm, I don't know this author.");
+        }
+
+        const data = await response.json();
+        console.log(data.results);
+        const quotes = data.results;
+        quotes.forEach(quote => {
+            console.log(quote.content);
+        });
+
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+fetchQuote()
