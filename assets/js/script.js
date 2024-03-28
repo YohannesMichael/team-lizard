3d35805c181e2231c85a864ccfba1f76d17a4d24
cloud444 <y_michael@outlook.com>
2024-03-29 02:17:51 +1100

    meanth author not genre lol

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const genreInput = document.getElementById('genre').value;
 
 const genreUrl = `https://openlibrary.org/search/${genreInput}.json`;
 
-async function fetchGenre() {
+async function fetchAuthor() {
     try {
         const response = await fetch('https://openlibrary.org/subjects/love.json');
         if (!response.ok) {
@@ -24,4 +24,4 @@ async function fetchGenre() {
     }
 }
 
-fetchGenre();
+fetchAuthor();
